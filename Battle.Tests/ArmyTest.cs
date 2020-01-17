b7d04f7e584a97bbe553e21a3f683c03e9f8f328
Sven Bex <sven.bex@cegeka.com>
2020-01-17 14:21:45 +0100

    US4

---
+++ Battle.Tests/ArmyTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Newtonsoft.Json.Bson;
+using Xunit;
+
+namespace Battle.Tests
+{
+    public class ArmyTest
+    {
+        [Fact]
+        public void Construction_NoSoldiersEnlisted()
+        {
+            var army = new Army();
+            army.EnlistedSoldiers.Should().HaveCount(0);
+        }
+
+        [Fact]
+        public void Enlist_GivenASoldier_SoldierEnlisted()
+        {
+            // GIVEN
+            var army = new Army();
+            var dirk = new Soldier("dirk");
+
+            // WHEN
+            army.Enlist(dirk);
+
+            // THEN
+            army.EnlistedSoldiers.Should().HaveCount(1);
+            army.EnlistedSoldiers.First().Should().Be(dirk);
+        }
+
+        [Fact]
+        public void Enlist_GivenTwoSoldiers_FirstSoldierShouldIsFrontman()
+        {
+            // GIVEN
+            var army = new Army();
+            var soldier1 = new Soldier("Bob");
+            var soldier2 = new Soldier("Bort");
+
+            // WHEN
+            army.Enlist(soldier1);
+            army.Enlist(soldier2);
+
+            // THEN
+            army.Frontman.Should().Be(soldier1);
+        }
+
+        [Fact]
+        public void Frontman_GivenNoSoldiersEnlisted_ThrowsException()
+        {
+            // GIVEN
+            var army = new Army();
+
+            // THEN
+            Assert.Throws<Exception>(() => army.Frontman);
+        }
+    }
+}
---
+++ Battle/Army.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Battle
+{
+    public class Army
+    {
+        private List<Soldier> _enlistedSoldiers = new List<Soldier>();
+        public IEnumerable<Soldier> EnlistedSoldiers => _enlistedSoldiers;
+
+        public Soldier Frontman
+        {
+            get 
+            {
+                return _enlistedSoldiers.Any() 
+                    ? _enlistedSoldiers.First() 
+                    : throw new Exception("We should draft more people");
+            }
+        }
+
+        public void Enlist(Soldier soldier)
+        {
+            _enlistedSoldiers.Add(soldier);
+        }
+    }
+}
